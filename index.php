f2814baf0ceefe7f3d1250d176f26ef429084baa
jakedurg <jmd0078@mix.wvu.edu>
2021-02-04 22:46:43 -0500

    Lab2 - About Page

---
+++ index.php
@@ -0,0 +1,51 @@
+<main>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us</h1>
+        <p>Here at We Love Birds we are passionate about avians. What started as just 5 guys in a garage talking about birds quickly turned into the premier
+            bird hub of the world. Send us your bird pics and we will rate them on the proprietary Bird-and-Avian-Ranking-System (BaAS)</p>
+    </div>
+    <div class="row">
+        <div class = "column">
+            <div class = "card">
+                <img src="images/JoelOsteen.jpg">
+                <div class="container">
+                    <h2>Spiritual Leader and Oddball</h2>
+                    <p>Joel Osteen</p>
+                    <p>Email: godsChosenOne@lakeside.com</p>
+                </div>
+            </div>
+        </div>
+        <div class = "column">
+            <div class = "card">
+                <img src="images/TomBrady.jpg" alt= "tommy Boy" height = "1000px" width="250px">
+                <div class="container">
+                    <h2>Mastermind and GOAT</h2>
+                    <p>Thomas Brady III</p>
+                    <p>Email: goat@rogersSucks.com</p>
+                </div>
+            </div>
+        </div>
+        <div class = "column">
+            <div class = "card">
+                <img src="images/DonaldGlover.jpg">
+                <div class="container">
+                    <h2>Jack of All Trades</h2>
+                    <p>Childish Glover</p>
+                    <p>Email: actorProducerRapper@complex.com</p>
+                </div>
+            </div>
+        </div>
+        <div class = "column">
+            <div class = "card">
+                <img src="images/ron.jpg">
+                <div class="container">
+                    <h2>Stud and Bird Fan</h2>
+                    <p>Ron Reaser</p>
+                    <p>Email: reaser@Ron.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
