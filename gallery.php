53e9517a0ede9aef03540159857cc18308f25661
jakedurg <jmd0078@mix.wvu.edu>
2021-04-10 15:00:07 -0400

    styling changes

--- gallery.php
+++ gallery.php
@@ -4,23 +4,27 @@ require 'includes/header.php';
 
 <main>
     <link rel="stylesheet" href="css/gallery.css">
-    <h1>Gallery</h1>
-    <div class="gallery-container">
-        <?php
-        include_once "includes/dbhandler.php";
-        $sql = "SELECT * FROM gallery ORDER BY upload_date DESC";
-        $query = mysqli_query($conn, $sql);
+    <div class="outer-bg-cover">
+        <div class="inner-bg-cover">
+            <h1>Gallery</h1>
+            <div class="gallery-container inner-bg">
+                <?php
+                include_once "includes/dbhandler.php";
+                $sql = "SELECT * FROM gallery ORDER BY upload_date DESC";
+                $query = mysqli_query($conn, $sql);
 
-        while($row = mysqli_fetch_assoc($query)){
-                echo '<div class="card">
-                <a href="review.php?id='.$row['pid'].'">
-                    <img src="'.$row["picpath"].'">
-                    <h3>'.$row["title"].'</h3>
-                    <p>'.$row["descript"].'</p>
-                </a>
-            </div>';
-        }
-        ?>
-        
+                while($row = mysqli_fetch_assoc($query)){
+                        echo '<div class="card">
+                        <a href="review.php?id='.$row['pid'].'">
+                            <img src="'.$row["picpath"].'">
+                            <h3>'.$row["title"].'</h3>
+                            <p>'.$row["descript"].'</p>
+                        </a>
+                    </div>';
+                }
+                ?>
+                
+            </div>
+        </div>
     </div>
 </main>
--- index.php
+++ index.php
@@ -2,52 +2,56 @@
 require 'includes/header.php';
 ?>
 
-<main>
+<main class="outer-bg-cover">
     <link rel="stylesheet" href="css/about.css">
-
-    <div class="about">
-        <h1>About Us</h1>
-        <p>Here at We Love Birds we are passionate about avians. What started as just 5 guys in a garage talking about birds quickly turned into the premier
-            bird hub of the world. Send us your bird pics and we will rate them on the proprietary Bird-and-Avian-Ranking-System (BaAS)</p>
-    </div>
-    <div class="row">
-        <div class = "column">
-            <div class = "card">
-                <img src="images/JoelOsteen.jpg">
-                <div class="container">
-                    <h2>Spiritual Leader and Oddball</h2>
-                    <p>Joel Osteen</p>
-                    <p>Email: godsChosenOne@lakeside.com</p>
+    <div class = "inner-bg-cover">
+        <div class="about">
+            <h1>About Us</h1>
+            <p>At TrailNerds group of similar minded mediocre hikers that wanted to create a space for us to all come together rate and review trails </p>
+        </div>
+        <div class="row">
+            <div class = "column">
+                <div class = "card">
+                    
+                    <div class="container">
+                        <img src="images/JoelOsteen.jpg">
+                        <h2>Spiritual Leader and Oddball</h2>
+                        <p>Joel Osteen</p>
+                        <p>Email: godsChosenOne@lakeside.com</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class = "column">
-            <div class = "card">
-                <img src="images/TomBrady.jpg" alt= "tommy Boy" height = "1000px" width="250px">
-                <div class="container">
-                    <h2>Mastermind and GOAT</h2>
-                    <p>Thomas Brady III</p>
-                    <p>Email: goat@rogersSucks.com</p>
+            <div class = "column">
+                <div class = "card">
+                    
+                    <div class="container">
+                        <img src="images/TomBrady.jpg" alt= "tommy Boy" height = "1000px" width="250px">
+                        <h2>Mastermind and GOAT</h2>
+                        <p>Thomas Brady III</p>
+                        <p>Email: goat@rogersSucks.com</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class = "column">
-            <div class = "card">
-                <img src="images/DonaldGlover.jpg">
-                <div class="container">
-                    <h2>Jack of All Trades</h2>
-                    <p>Childish Glover</p>
-                    <p>Email: actorProducerRapper@complex.com</p>
+            <div class = "column">
+                <div class = "card">
+                    
+                    <div class="container">
+                        <img src="images/DonaldGlover.jpg">
+                        <h2>Jack of All Trades</h2>
+                        <p>Childish Glover</p>
+                        <p>Email: actorProducerRapper@complex.com</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class = "column">
-            <div class = "card">
-                <img src="images/ron.jpg">
-                <div class="container">
-                    <h2>Stud and Bird Fan</h2>
-                    <p>Ron Reaser</p>
-                    <p>Email: reaser@Ron.com</p>
+            <div class = "column">
+                <div class = "card">
+                    
+                    <div class="container">
+                        <img src="images/ron.jpg">
+                        <h2>Stud and Bird Fan</h2>
+                        <p>Ron Reaser</p>
+                        <p>Email: reaser@Ron.com</p>
+                    </div>
                 </div>
             </div>
         </div>
